a95342935197c3dedea41d50b47276ed4a8d15fd
Dagmar Cirino <dagmar.mv@gmail.com>
2022-02-10 01:25:42 -0300

    adicionando mock, diretivas e atribuição de valor

---
+++ src/app/mock.heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero.model";
+
+export const HEROES: Hero[] = [
+	{id: 1, name: 'Windstorm'},
+	{id: 2, name: 'Hulk'},
+	{id: 3, name: 'Magneta'},
+	{id: 4, name: 'Tornado'},
+	{id: 5, name: 'Spideman'},
+	{id: 6, name: 'Batman'},
+	{id: 7, name: 'Wolverine'},
+	{id: 8, name: 'Superman'},
+	{id: 9, name: 'Tornado'},
+	{id: 10, name: 'Thor'},
+];
