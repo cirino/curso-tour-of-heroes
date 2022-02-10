f7a8150edfbf76b654718a1552bcb961bf172cd1
Dagmar Cirino <dagmar.mv@gmail.com>
2022-02-10 02:01:11 -0300

    add component detalhe e tirando responsabilidade

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,10 +8,6 @@ import { HEROES } from '../mock.heroes';
 	styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent {
-	hero: Hero = {
-		id: 1,
-		name: "Spiderman"
-	};
 
 	heroes = HEROES;
 	selectedHero?: Hero;
